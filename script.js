01c6e85cd5e4f178fe79a283b831af1db8816f44
Lucifer420420 <abhinavsisodia2027@gmail.com>
2025-01-22 15:23:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function response(answer) {
     let responseMessage = document.getElementById("responseMessage");
 
     if (answer === 'yes') {
-        responseMessage.textContent = "YAY! I am so happy! I love you, Buggu!";
+        responseMessage.textContent = "YAY! I am so happy! I love you, Mantasha! This is just the beginning of our beautiful journey together.";
     } else {
-        responseMessage.textContent = "It's okay, Buggu! You're still my everything!";
+        responseMessage.textContent = "It's okay, Mantasha! No matter what, you're still the love of my life.";
     }
 
     overlay.style.display = "flex";
